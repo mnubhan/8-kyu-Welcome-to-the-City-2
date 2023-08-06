af1641d0db4c622d7a97236535461a421d892bbe
mnubhan <76425959+mnubhan@users.noreply.github.com>
2023-08-06 21:39:04 +0100

    Update answer.py

--- answer.py
+++ answer.py
@@ -1,2 +1,5 @@
 def say_hello(name, city, state):
     return f"Hello, {' '.join(name)}! Welcome to {city}, {state}!"
+
+def say_hello(name, city, state):
+  return "Hello, {}! Welcome to {}, {}!".format(" ".join(name), city, state)
